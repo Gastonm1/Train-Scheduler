efc16adb7d362b9bb2a6307778c5cbe4c3c0c8b2
Gastonm1 <gaston.marques@gmail.com>
2019-01-18 16:10:24 -0600

    database config

--- assets/JS/trainSchedule.js
+++ assets/JS/trainSchedule.js
@@ -8,9 +8,28 @@
 //-----------------------------------------------------------------------------------------------
 $(document).ready(function() {
 
+// 1. Initialize Firebase
 
+    var config = {
+        apiKey: "AIzaSyBJ22lSRMAeepuElvdeKpV8MnCjAHpqJjE",
+        authDomain: "train-scheduler-311e9.firebaseapp.com",
+        databaseURL: "https://train-scheduler-311e9.firebaseio.com",
+        projectId: "train-scheduler-311e9",
+        storageBucket: "train-scheduler-311e9.appspot.com",
+        messagingSenderId: "78321187755"
+      };
 
+      var database = firebase.initializeApp(config);
 
+//2. Create a button for adding the new trains > update the html & update the firebase database
+ $("#add-train-btn").on("click", function(event) {
+     event.preventDefault();
+
+     var tName = $("#InputTrain").val().trim();
+     var tDestination = $("#InputDestination").val().trim();
+     var tTime = $("#InputTraintime").val().trim();
+     var tFrequency = $("#InputFrequency").val().trim();
+ }
 
 
 });
