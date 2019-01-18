415a495f40005afb2de7f64e5b17604fa5622e2a
Gastonm1 <gaston.marques@gmail.com>
2019-01-18 15:40:37 -0600

    Psudo Code

--- assets/JS/trainSchedule.js
+++ assets/JS/trainSchedule.js
@@ -1,4 +1,16 @@
+// Instructions:
+// 1. Initialize Firebase
+//2. Create a button for adding the new trains > update the html & update the firebase database
+// 3. Create a way to retrieve trains from the train database.
+//4. Create a way to calculate the next arrival time.
+//5. Calculate Minutes away
+
+//-----------------------------------------------------------------------------------------------
 $(document).ready(function() {
-//----------------------------------------------------------------------------
+
+
+
+
+
 
 });
